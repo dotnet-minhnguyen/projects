52b797083ba5da5c6712c6d340ab4d4e29a9a291
Bing Tran <binhtran@bbv.ch>
2021-06-14 19:17:05 +0700

    add ssh

---
+++ ssh/login.sh
@@ -0,0 +1,22 @@
+eval $(ssh-agent -s)
+ssh-add phuminh88
+Saigon@020
+
+
+git config --global user.name phuminh88
+git config --global user.email phuminh88
+
+git config user.name phuminh88
+git config user.email phuminh88
+
+git clone git@github.com:dungdinhtien/ZERONINE.Nutrition.Web.git
+cd ZERONINE.Nutrition.Web
+yarn
+
+
+
+git config --global user.name phuminh
+git config --global user.email phuminh@maysoft.vn
+
+git config user.name phuminh
+git config user.email phuminh@maysoft.vn
